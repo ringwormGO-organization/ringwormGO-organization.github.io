53900b3f2308243b87712243d6683541fe080309
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2022-02-13 13:25:23 +0100

    Theme changer

--- ringwormGO-website/main.js
+++ ringwormGO-website/main.js
@@ -1,6 +1,34 @@
-var ready = "Ready";
+/* Variables */
+var background_color = document.getElementById("back");
 
+/* Functions */
 document.onreadystatechange = () =>
 {
-    console.log(ready);
+    console.log("Ready");
+    main();
+}
+
+function main()
+{
+    console.log("main function");
+}
+
+function theme()
+{
+    if (background_color.style.backgroundColor != "black")
+        var isDarkTheme = false;
+    else
+        var isDarkTheme = true;
+
+    if (isDarkTheme == false)
+    {
+        background_color.style.backgroundColor = "black";
+        return isDarkTheme = true;
+    }
+
+    if (isDarkTheme == true)
+    {
+        background_color.style.backgroundColor = "grey";
+        return isDarkTheme = false;
+    }
 }
