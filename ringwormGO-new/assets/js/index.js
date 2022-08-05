70d1028dc67502479deeb66f114b35966e3d9c2c
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2022-08-05 12:12:06 +0200

    Themes update on new website

--- ringwormGO-new/assets/js/index.js
+++ ringwormGO-new/assets/js/index.js
@@ -1,8 +1,16 @@
+var current_theme = 'dark';
+
 function Theme()
 {
     let new_theme = null;
 
     new_theme = randomTheme();
+
+    if (new_theme == current_theme)
+    {
+        Theme();
+    }
+
     console.log("New theme is: '" + new_theme + "'!");
     switch (new_theme)
     {
@@ -12,6 +20,9 @@ function Theme()
 
             document.documentElement.style.setProperty('--bg-accordion-color', 'black');
             document.documentElement.style.setProperty('--fg-accordion-color', 'white');
+
+            document.documentElement.style.setProperty('--fg-accordion-not-collapsed-color', 'yellowgreen');
+            document.documentElement.style.setProperty('--border-accordion', 'black solid 1px');
             break;
 
         case 'dark':
@@ -20,6 +31,9 @@ function Theme()
 
             document.documentElement.style.setProperty('--bg-accordion-color', 'white');
             document.documentElement.style.setProperty('--fg-accordion-color', 'black');
+
+            document.documentElement.style.setProperty('--fg-accordion-not-collapsed-color', '#ff9800');
+            document.documentElement.style.setProperty('--border-accordion', 'none');
             break;
 
         case 'yellowlight':
@@ -28,6 +42,12 @@ function Theme()
 
             document.documentElement.style.setProperty('--bg-accordion-color', 'white');
             document.documentElement.style.setProperty('--fg-accordion-color', 'black');
+
+            document.documentElement.style.setProperty('--fg-accordion-not-collapsed-color', 'yellowgreen');
+            document.documentElement.style.setProperty('--border-accordion', 'fuchsia solid 1px');
+
+            document.documentElement.style.setProperty('--fg-accordion-not-collapsed-color', '#ff9800');
+            document.documentElement.style.setProperty('--border-accordion', 'none');
             break;
 
         case 'darkred':
@@ -36,6 +56,9 @@ function Theme()
 
             document.documentElement.style.setProperty('--bg-accordion-color', 'white');
             document.documentElement.style.setProperty('--fg-accordion-color', 'black');
+
+            document.documentElement.style.setProperty('--fg-accordion-not-collapsed-color', '#ff9800');
+            document.documentElement.style.setProperty('--border-accordion', 'none');
             break;
 
         case 'darkblue':
@@ -44,15 +67,31 @@ function Theme()
 
             document.documentElement.style.setProperty('--bg-accordion-color', 'white');
             document.documentElement.style.setProperty('--fg-accordion-color', 'black');
+
+            document.documentElement.style.setProperty('--fg-accordion-not-collapsed-color', '#ff9800');
+            document.documentElement.style.setProperty('--border-accordion', 'none');
+            break;
+
+        case 'darkgreen':
+            document.documentElement.style.setProperty('--bg-color', 'darkgreen');
+            document.documentElement.style.setProperty('--fg-color', 'white');
+
+            document.documentElement.style.setProperty('--bg-accordion-color', 'white');
+            document.documentElement.style.setProperty('--fg-accordion-color', 'black');
+
+            document.documentElement.style.setProperty('--fg-accordion-not-collapsed-color', '#ff9800');
+            document.documentElement.style.setProperty('--border-accordion', 'none');
             break;
 
         default:
             break;
     }
+    
+    current_theme = new_theme;
 }
 
 function randomTheme()
 {
-    const themes = ['light', 'dark', 'yellowlight', 'darkred', 'darkblue'];
+    const themes = ['light', 'dark', 'yellowlight', 'darkred', 'darkblue', 'darkgreen'];
     return themes[Math.floor(Math.random() * themes.length)];
 }
