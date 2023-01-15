fea04cd167f2026e26be15c5a00f17187fc28427
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2023-01-15 14:50:09 +0100

    Languages chart and some refactoring

---
+++ src/new/assets/js/chart.js
@@ -0,0 +1,89 @@
+function CalculatePercentage(num) {
+    const number_of_repos = 17;
+
+    return (num / number_of_repos) * 100;
+}
+
+var languages = new Map();
+
+languages.set("VB.NET", [CalculatePercentage(1), "#945db7"]);
+languages.set("Python", [CalculatePercentage(5), "#3572a5"]);
+languages.set("C++", [CalculatePercentage(7), "#f34b7d"]);
+languages.set("C#", [CalculatePercentage(2), "#178600"]);
+languages.set("Inno Setup", [CalculatePercentage(1), "#264B99"]);
+languages.set("Without language", [CalculatePercentage(2), "gray"]);
+languages.set("PHP", [CalculatePercentage(1), "#4F5D95"]);
+languages.set("C", [CalculatePercentage(3), "#555555"]);
+languages.set("HTML", [CalculatePercentage(2), "#E34C26"]);
+languages.set("Lua", [CalculatePercentage(1), "#000080"]);
+languages.set("Javascript", [CalculatePercentage(1), "#F1E05A"]);
+
+const mapArray = Array.from(languages);
+mapArray.sort((a, b) => b[1][0] - a[1][0]);
+const sortedMap = new Map(mapArray);
+
+const firstStrings = Array.from(sortedMap.entries()).map(
+    ([key, _value]) => key
+);
+const numbers = Array.from(sortedMap.entries()).map(
+    ([_key, value]) => value[0]
+);
+const secondStrings = Array.from(sortedMap.entries()).map(
+    ([_key, value]) => value[1]
+);
+
+let numbers_two = [];
+
+for (let index = 0; index < numbers.length; index++) {
+    const element = numbers[index];
+
+    numbers_two.push((Math.round(element * 100) / 100).toString() + "%");
+}
+
+/*
+ * Thanks for ChatGPT for this part
+ * Get a reference to the canvas element
+ */
+const canvas = document.getElementById("chart");
+const ctx = canvas.getContext("2d");
+
+/*
+ * Thanks for ChatGPT for this part
+ * Create a new pie chart using Chart.js
+ */
+const chart = new Chart(ctx, {
+    type: "pie",
+    data: {
+        labels: firstStrings,
+        datasets: [
+            {
+                data: numbers,
+                backgroundColor: secondStrings,
+            },
+        ],
+    },
+    options: {
+        animation: {
+            animateRotate: true,
+            animateScale: true,
+        },
+        tooltips: {
+            callbacks: {
+                label: function (tooltipItems, data) {
+                    return (
+                        data.labels[tooltipItems.index] +
+                        " : " +
+                        (
+                            Math.round(
+                                data.datasets[tooltipItems.datasetIndex].data[
+                                tooltipItems.index
+                                ] * 100
+                            ) / 100
+                        ).toString() +
+                        " %"
+                    );
+                },
+            },
+        },
+    },
+});
--- src/new/assets/js/index.js
+++ src/new/assets/js/index.js
@@ -1,97 +1,188 @@
-var current_theme = 'dark';
+/* Thanks to Fireship for the code */
 
-function Theme()
-{
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } else {
+            entry.target.classList.remove("show");
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+
+/* ----------------------------------- */
+
+var current_theme = "dark";
+
+function Theme() {
     let new_theme = null;
 
     new_theme = randomTheme();
 
-    if (new_theme == current_theme)
-    {
+    if (new_theme == current_theme) {
         Theme();
     }
 
     console.log("New theme is: '" + new_theme + "'!");
-    switch (new_theme)
-    {
-        case 'light':
-            document.documentElement.style.setProperty('--bg-color', 'white')
-            document.documentElement.style.setProperty('--fg-color', 'black')
+    switch (new_theme) {
+        case "light":
+            document.documentElement.style.setProperty("--bg-color", "white");
+            document.documentElement.style.setProperty("--fg-color", "black");
 
-            document.documentElement.style.setProperty('--bg-accordion-color', 'black');
-            document.documentElement.style.setProperty('--fg-accordion-color', 'white');
+            document.documentElement.style.setProperty(
+                "--bg-accordion-color",
+                "black"
+            );
+            document.documentElement.style.setProperty(
+                "--fg-accordion-color",
+                "white"
+            );
 
-            document.documentElement.style.setProperty('--fg-accordion-not-collapsed-color', 'yellowgreen');
-            document.documentElement.style.setProperty('--border-accordion', 'black solid 1px');
+            document.documentElement.style.setProperty(
+                "--fg-accordion-not-collapsed-color",
+                "yellowgreen"
+            );
+            document.documentElement.style.setProperty(
+                "--border-accordion",
+                "black solid 1px"
+            );
             break;
 
-        case 'dark':
-            document.documentElement.style.setProperty('--bg-color', 'var(--bs-gray-dark)')
-            document.documentElement.style.setProperty('--fg-color', 'white')
+        case "dark":
+            document.documentElement.style.setProperty(
+                "--bg-color",
+                "var(--bs-gray-dark)"
+            );
+            document.documentElement.style.setProperty("--fg-color", "white");
 
-            document.documentElement.style.setProperty('--bg-accordion-color', 'white');
-            document.documentElement.style.setProperty('--fg-accordion-color', 'black');
+            document.documentElement.style.setProperty(
+                "--bg-accordion-color",
+                "white"
+            );
+            document.documentElement.style.setProperty(
+                "--fg-accordion-color",
+                "black"
+            );
 
-            document.documentElement.style.setProperty('--fg-accordion-not-collapsed-color', '#ff9800');
-            document.documentElement.style.setProperty('--border-accordion', 'none');
+            document.documentElement.style.setProperty(
+                "--fg-accordion-not-collapsed-color",
+                "#ff9800"
+            );
+            document.documentElement.style.setProperty("--border-accordion", "none");
             break;
 
-        case 'yellowlight':
-            document.documentElement.style.setProperty('--bg-color', 'rgb(180, 178, 151)');
-            document.documentElement.style.setProperty('--fg-color', 'white');
+        case "yellowlight":
+            document.documentElement.style.setProperty(
+                "--bg-color",
+                "rgb(180, 178, 151)"
+            );
+            document.documentElement.style.setProperty("--fg-color", "white");
 
-            document.documentElement.style.setProperty('--bg-accordion-color', 'white');
-            document.documentElement.style.setProperty('--fg-accordion-color', 'black');
+            document.documentElement.style.setProperty(
+                "--bg-accordion-color",
+                "white"
+            );
+            document.documentElement.style.setProperty(
+                "--fg-accordion-color",
+                "black"
+            );
 
-            document.documentElement.style.setProperty('--fg-accordion-not-collapsed-color', 'yellowgreen');
-            document.documentElement.style.setProperty('--border-accordion', 'fuchsia solid 1px');
+            document.documentElement.style.setProperty(
+                "--fg-accordion-not-collapsed-color",
+                "yellowgreen"
+            );
+            document.documentElement.style.setProperty(
+                "--border-accordion",
+                "fuchsia solid 1px"
+            );
 
-            document.documentElement.style.setProperty('--fg-accordion-not-collapsed-color', '#ff9800');
-            document.documentElement.style.setProperty('--border-accordion', 'none');
+            document.documentElement.style.setProperty(
+                "--fg-accordion-not-collapsed-color",
+                "#ff9800"
+            );
+            document.documentElement.style.setProperty("--border-accordion", "none");
             break;
 
-        case 'darkred':
-            document.documentElement.style.setProperty('--bg-color', 'darkred');
-            document.documentElement.style.setProperty('--fg-color', 'white');
+        case "darkred":
+            document.documentElement.style.setProperty("--bg-color", "darkred");
+            document.documentElement.style.setProperty("--fg-color", "white");
 
-            document.documentElement.style.setProperty('--bg-accordion-color', 'white');
-            document.documentElement.style.setProperty('--fg-accordion-color', 'black');
+            document.documentElement.style.setProperty(
+                "--bg-accordion-color",
+                "white"
+            );
+            document.documentElement.style.setProperty(
+                "--fg-accordion-color",
+                "black"
+            );
 
-            document.documentElement.style.setProperty('--fg-accordion-not-collapsed-color', '#ff9800');
-            document.documentElement.style.setProperty('--border-accordion', 'none');
+            document.documentElement.style.setProperty(
+                "--fg-accordion-not-collapsed-color",
+                "#ff9800"
+            );
+            document.documentElement.style.setProperty("--border-accordion", "none");
             break;
 
-        case 'darkblue':
-            document.documentElement.style.setProperty('--bg-color', 'darkblue');
-            document.documentElement.style.setProperty('--fg-color', 'white');
+        case "darkblue":
+            document.documentElement.style.setProperty("--bg-color", "darkblue");
+            document.documentElement.style.setProperty("--fg-color", "white");
 
-            document.documentElement.style.setProperty('--bg-accordion-color', 'white');
-            document.documentElement.style.setProperty('--fg-accordion-color', 'black');
+            document.documentElement.style.setProperty(
+                "--bg-accordion-color",
+                "white"
+            );
+            document.documentElement.style.setProperty(
+                "--fg-accordion-color",
+                "black"
+            );
 
-            document.documentElement.style.setProperty('--fg-accordion-not-collapsed-color', '#ff9800');
-            document.documentElement.style.setProperty('--border-accordion', 'none');
+            document.documentElement.style.setProperty(
+                "--fg-accordion-not-collapsed-color",
+                "#ff9800"
+            );
+            document.documentElement.style.setProperty("--border-accordion", "none");
             break;
 
-        case 'darkgreen':
-            document.documentElement.style.setProperty('--bg-color', 'darkgreen');
-            document.documentElement.style.setProperty('--fg-color', 'white');
+        case "darkgreen":
+            document.documentElement.style.setProperty("--bg-color", "darkgreen");
+            document.documentElement.style.setProperty("--fg-color", "white");
 
-            document.documentElement.style.setProperty('--bg-accordion-color', 'white');
-            document.documentElement.style.setProperty('--fg-accordion-color', 'black');
+            document.documentElement.style.setProperty(
+                "--bg-accordion-color",
+                "white"
+            );
+            document.documentElement.style.setProperty(
+                "--fg-accordion-color",
+                "black"
+            );
 
-            document.documentElement.style.setProperty('--fg-accordion-not-collapsed-color', '#ff9800');
-            document.documentElement.style.setProperty('--border-accordion', 'none');
+            document.documentElement.style.setProperty(
+                "--fg-accordion-not-collapsed-color",
+                "#ff9800"
+            );
+            document.documentElement.style.setProperty("--border-accordion", "none");
             break;
 
         default:
             break;
     }
-    
+
     current_theme = new_theme;
 }
 
-function randomTheme()
-{
-    const themes = ['light', 'dark', 'yellowlight', 'darkred', 'darkblue', 'darkgreen'];
+function randomTheme() {
+    const themes = [
+        "light",
+        "dark",
+        "yellowlight",
+        "darkred",
+        "darkblue",
+        "darkgreen",
+    ];
     return themes[Math.floor(Math.random() * themes.length)];
 }
