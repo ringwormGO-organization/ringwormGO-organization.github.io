39dbb131274ba645ff815f89550332410ab2ad9f
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2023-01-29 21:07:40 +0100

    Update

--- src/new/assets/js/index.js
+++ src/new/assets/js/index.js
@@ -1,22 +1,3 @@
-/* Thanks to Fireship for the code */
-
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry);
-
-        if (entry.isIntersecting) {
-            entry.target.classList.add("show");
-        } else {
-            entry.target.classList.remove("show");
-        }
-    });
-});
-
-const hiddenElements = document.querySelectorAll(".hidden");
-hiddenElements.forEach((el) => observer.observe(el));
-
-/* ----------------------------------- */
-
 var current_theme = "dark";
 
 function Theme() {
